d6d8156e35664ec0fb1b638994eb76c78131fc5c
leomet07 <leomet07@gmail.com>
2022-01-30 14:39:11 -0500

    Added webcam test scripts

---
+++ install_cpp_webcam_test.sh
@@ -0,0 +1,2 @@
+cd ~/Desktop
+git clone https://github.com/leomet07/cpp-motion cpp_webcam_test
---
+++ install_py_webcam_test.sh
@@ -0,0 +1,4 @@
+cd ~/Desktop
+mkdir py_webcam_test
+cd py_webcam_test
+wget https://gist.githubusercontent.com/leomet07/fff360bf812df1a32b6be9e6a639b30b/raw/4e8315f4ee8e17825a32b80f4df083dce2f68c8a/py_webcam_test.py -O main.py
